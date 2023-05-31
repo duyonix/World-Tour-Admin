a3b6be2cc0408b4567ec78ea1cab0c1d15de5797
duyonix <dtduy2210@gmail.com>
2023-05-31 12:09:57 +0700

    done ReviewInput

---
+++ src/components/ReviewInput/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./style.scss";
+import { Button, Input } from "antd";
+import {
+  YoutubeFilled,
+  EyeFilled,
+  EyeInvisibleFilled
+} from "@ant-design/icons";
+
+type Props = {
+  value?: string;
+  onChange?: any;
+};
+
+const ReviewInput = ({ value, onChange }: Props) => {
+  const handleReview = () => {
+    if (value) {
+      window.open(value, "_blank");
+    }
+  };
+
+  return (
+    <div className="d-flex review-input">
+      <Input
+        className="input-link"
+        prefix={<YoutubeFilled style={{ fontSize: "20px" }} />}
+        onChange={onChange}
+        value={value}
+        style={{ flexGrow: 1 }}
+      />
+      <Button
+        onClick={handleReview}
+        className="form-item-icon"
+        icon={
+          value ? (
+            <EyeFilled style={{ fontSize: "20px" }} />
+          ) : (
+            <EyeInvisibleFilled style={{ fontSize: "20px" }} />
+          )
+        }
+        disabled={!value}
+      />
+    </div>
+  );
+};
+
+export default ReviewInput;
